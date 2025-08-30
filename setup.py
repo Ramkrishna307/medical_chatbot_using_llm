47e6511c2a4de8ee25d289c12ab46e185ab4d1d1
Ramkrishna Roy <ramkrishnaroy324@gmail.com>
2025-08-30 11:44:23 +0530

    reuirements added
    Signed-off-by: Ramkrishna Roy <ramkrishnaroy324@gmail.com>

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="mdecical_chatbot",
+    version="0.1.0",
+    author="Ramkrishna Roy",
+    author_email="ramkrishnaroy624@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
